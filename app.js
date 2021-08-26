7b79715d121ab5c2c60538a9208f108c24bc036f
Cyberkrane <cyberkrane@gmail.com>
2021-08-26 14:09:44 -0300

    subida de archivos iniciales
    adada

---
+++ app.js
@@ -0,0 +1,4 @@
+function myFunction() {
+
+    document.querySelector('.contenedor').innerHTML = '<img class="fade-in-image" src="./src/imgs/llorona.jpg" alt=""><audio autoplay src="./src/gru.mp3"></audio>';
+}
